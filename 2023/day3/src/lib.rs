fac922fb454cc2c3f3c6bee7985d7db82a680a7f
koskeller <koskeller@gmail.com>
2023-12-18 05:59:13 +0100

    refactor day 3

--- 2023/day3/src/lib.rs
+++ 2023/day3/src/lib.rs
@@ -1,42 +1,49 @@
 #![allow(unused)]
 
+/// This makes two assumptions that ended up being true for the input:
+/// - No number touches more than one symbol
+/// - No non-star symbol touches exactly two numbers
 use regex::Regex;
 use std::{collections::HashMap, ops::Range};
 
 fn part_one(input: &str) -> usize {
-    get_schema(input)
+    get_schematic(input)
         .into_iter()
         .map(|(_, v)| v.iter().sum::<usize>())
         .sum()
 }
 
 fn part_two(input: &str) -> usize {
-    get_schema(input)
+    get_schematic(input)
         .into_iter()
         .filter(|(_, v)| v.len() == 2)
         .map(|(_, v)| v.iter().fold(1, |acc, &x| acc * x))
         .sum()
 }
 
-fn get_schema(input: &str) -> HashMap<(i32, i32), Vec<usize>> {
-    let mut set = parse_board(input);
+fn get_schematic(input: &str) -> HashMap<(i32, i32), Vec<usize>> {
+    let mut hm = parse_schematic(input);
+
     let re = Regex::new(r"\d+").unwrap();
+
     for (line_index, line) in input.lines().enumerate() {
         for number in re.find_iter(line) {
             let edges = get_edges(line_index as i32, number.range());
+            let number_value = number.as_str().parse::<usize>().unwrap();
             for edge in &edges {
-                if set.contains_key(edge) {
-                    set.entry(*edge)
-                        .and_modify(|v| v.push(number.as_str().parse::<usize>().unwrap()));
+                if let Some(v) = hm.get_mut(edge) {
+                    v.push(number_value);
                     break;
                 }
             }
         }
     }
-    set
+
+    hm
 }
 
-fn parse_board(input: &str) -> HashMap<(i32, i32), Vec<usize>> {
+// Coordinates of all symbols mapped to values of adjusted numbers.
+fn parse_schematic(input: &str) -> HashMap<(i32, i32), Vec<usize>> {
     let mut set = HashMap::new();
     for (line_index, line) in input.lines().enumerate() {
         for (char_index, char) in line.chars().enumerate() {
@@ -48,33 +55,13 @@ fn parse_board(input: &str) -> HashMap<(i32, i32), Vec<usize>> {
     set
 }
 
-// fn get_edges(line: i32, pos: i32) -> Vec<(i32, i32)> {
-//     vec![
-//         (line - 1, pos - 1),
-//         (line - 1, pos),
-//         (line - 1, pos + 1),
-//         (line, pos - 1),
-//         (line, pos + 1),
-//         (line + 1, pos - 1),
-//         (line + 1, pos),
-//         (line + 1, pos + 1),
-//     ]
-// }
-
+// Coordinates of all edges.
 fn get_edges(line: i32, range: Range<usize>) -> Vec<(i32, i32)> {
-    let min = range.clone().min().unwrap() as i32;
-    let max = range.clone().max().unwrap() as i32;
-    let mut edges = vec![
-        (line - 1, min - 1),
-        (line - 1, max + 1),
-        (line, min - 1),
-        (line, max + 1),
-        (line + 1, min - 1),
-        (line + 1, max + 1),
-    ];
-    for n in range {
-        edges.push((line - 1, n as i32));
-        edges.push((line + 1, n as i32));
+    let min = range.start as i32 - 1;
+    let max = range.end as i32;
+    let mut edges = vec![(line, min), (line, max)];
+    for n in min..=max {
+        edges.extend([(line - 1, n as i32), (line + 1, n as i32)]);
     }
     edges
 }
@@ -85,7 +72,7 @@ mod tests {
 
     #[test]
     fn test_parse_board() {
-        let board = parse_board("...*......\n..35..633.\n......#...");
+        let board = parse_schematic("...*......\n..35..633.\n......#...");
         assert_eq!(board.len(), 2);
         assert!(board.contains_key(&(0, 3)));
         assert!(board.contains_key(&(2, 6)));
