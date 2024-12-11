f201d7afedce8858800ce4e38f555788fece3748
koskeller <koskeller@gmail.com>
2024-12-11 20:30:51 +0100

    feat: day 10 part 2

--- 2024/10/src/main.rs
+++ 2024/10/src/main.rs
@@ -4,8 +4,9 @@ use std::collections::HashSet;
 use utils::Grid;
 
 fn main() {
-    let s = include_str!("input");
-    println!("{}", part_one(s.to_owned()));
+    let s = include_str!("input").to_owned();
+    println!("{}", part_one(s.clone()));
+    println!("{}", part_two(s.clone()));
 }
 
 fn part_one(input: String) -> usize {
@@ -38,6 +39,39 @@ fn part_one(input: String) -> usize {
             }
         }
         answer += summits.len();
+    }
+
+    return answer;
+}
+
+fn part_two(input: String) -> usize {
+    let mut answer = 0;
+    let grid = Grid::new(input.to_owned());
+
+    // Starts at height 0, ends at height 9, and always increases by a height of exactly 1 at each step.
+    let mut trailheads: Vec<(usize, usize)> = vec![];
+
+    for row in 0..grid.rows() {
+        for col in 0..grid.cols() {
+            if grid.get(row, col) == 0 {
+                trailheads.push((row, col));
+            }
+        }
+    }
+
+    for (row, col) in trailheads {
+        let mut paths = vec![(row, col)];
+
+        while let Some((row, col)) = paths.pop() {
+            let cur = grid.get(row, col);
+            for (r, c) in grid.neighbors(row, col) {
+                if cur == 8 && grid.get(r, c) == 9 {
+                    answer += 1;
+                } else if grid.get(r, c) == cur + 1 {
+                    paths.push((r, c));
+                }
+            }
+        }
     }
 
     return answer;
